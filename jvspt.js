3c467e1fb18fa4c3188edcb43b43ea059c37d2bc
Duldule Usaid <dulduleusaid5@gmail.com>
2024-08-18 08:09:39 +0530

    new made

---
+++ jvspt.js
@@ -0,0 +1,8 @@
+function toggMenu() {
+    const menu=document.querySelector(".menu-link");
+    const icon=document.querySelector(".togg-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+
+}
