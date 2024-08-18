0c79a416cc188374c42b24e5b86da66634d7b2fc
Duldule Usaid <dulduleusaid5@gmail.com>
2024-08-18 17:25:07 +0530

    updates

--- jvspt.js
+++
@@ -1,8 +0,0 @@
-function toggMenu() {
-    const menu=document.querySelector(".menu-link");
-    const icon=document.querySelector(".togg-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-
-
-}
